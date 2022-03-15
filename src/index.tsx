091d21c541ffd109bbcda0f9bb45d8fb1c574524
Nikita_Koryushkin <nickkorya@gmail.com>
2022-03-15 18:21:49 +0300

    Диплом. Верстка

--- src/index.tsx
+++ src/index.tsx
@@ -9,23 +9,3 @@ import ReactDOM from 'react-dom';
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-
-//<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="keywords" content="music, spotify, DONT BlOCK SPOTFY IN RUSSIA" />
-<meta name="description" content="Spotify redesign" />
-<link href="./index.css" rel="stylesheet" type="text/css" />
-<title>Spotify</title>
-</head>
-<body>
-	<header>
-	</header>
-//<img src="img/spotify-logo.svg" title="Spotify logo" alt="Spotify logo"></img>
-
-	<footer>
-
-	</footer>
-</body>
-</html>
